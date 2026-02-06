4c767fe7a8628356c26fed65d3c4c73e1893bb68
Milo Canseco <canseco3rd@gmail.com>
2026-02-06 03:00:17 -0700

    Added a Home route.

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Home Route
+app.get("/", (req, res) => {
+  res.send({
+    message: "Welcome to Doxa Cleaning llc API!",
+    version: "1.0.0",
+    endpoints: {
+      health: "/api/health",
+      docs: "Coming Soon...",
+    },
+  });
+});
+
 // Simple health check route
 app.get("/api/health", (req, res) => {
   res.send({
