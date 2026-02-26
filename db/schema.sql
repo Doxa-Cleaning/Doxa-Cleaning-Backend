8137b3a3a07d7c3344a9bb39898a6c502e4ce41f
Milo Canseco <canseco3rd@gmail.com>
2026-02-25 18:11:14 -0700

    Fixed bugs.

--- db/schema.sql
+++ db/schema.sql
@@ -29,13 +29,17 @@ CREATE TABLE jobs (
     id          serial PRIMARY KEY,
     employee_id integer NOT NULL,
     customer_id integer NOT NULL,
+    job_name        VARCHAR(50) NOT NULL,
     status      VARCHAR(50) NOT NULL,
     scheduled_date DATE NOT NULL,
-    scheduled_time SELECT CONVERT(VARCHAR[(15)], CAST('00:00' AS TIME), 100) NOT NULL,
+    scheduled_time TIME NOT NULL,
     estimated_duration INTEGER DEFAULT 60, -- in minutes
     created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     completed_at TIMESTAMP,
 
     FOREIGN KEY (employee_id) REFERENCES users(id),
-    FOREIGN KEY (customer_id) REFERENCES customers(id)
+    FOREIGN KEY (customer_id) REFERENCES customers(id),
+    FOREIGN KEY (job_name) REFERENCES customers(name)
 );
+
+SELECT scheduled_time FROM jobs;
